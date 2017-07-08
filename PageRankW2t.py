d177470a2e1041ae9149085a5057798ef43f242b
Sushant Pritmani <whisprrind.twister@gmail.com>
2017-07-08 17:49:43 -0400

    PageRank Completed

---
+++ PageRankW2t.py
@@ -0,0 +1,121 @@
+# Created by Sushant
+
+from collections import defaultdict
+from sets import Set
+import operator
+from math import log
+
+# Code for Inlinks File
+
+f = open('wt2g_inlinks.txt', 'r')
+l = f.readlines()
+
+# print l.__len__()
+
+# data Structures:
+# M is the dictionary containing the docID as the key and value as the inlinks to that ID
+# L is the dictionary where docID as the key and value is number of outlinks for given DOCID
+# PR is dictionary containing pagerank for a specific page
+# newPR is a dictionary used as temp variable for pagerank
+# P is the set of all pages
+# S is the set of sink nodes, i.e., pages that have no out links
+# perplexity is the list of all perplexity values used to calculate convergence
+
+M = {}
+L = {}
+PR = {}
+newPR = {}
+P = Set()
+S = Set()
+perplexityList  =[]
+
+# Function Def
+
+def getPValue():
+    entropy =0.0
+    for pg in PR.values():
+        entropy += pg*log(pg,2)
+
+    return 2**(-1.0*entropy)
+
+def isConverged(count):
+    pValue  = getPValue()
+    print str(count+1) + " " + str(pValue)
+    perplexityList.append(pValue)
+    if len(perplexityList)>4:
+        if((int(perplexityList[count]))==(int(perplexityList[count-1]))==(int(perplexityList[count-2]))==(int(perplexityList[count-3]))):
+
+            return True
+        else:
+            return False
+
+    else:
+        return False
+
+
+# Intialising M dictionary with given file
+for line in l:
+    line = line.split()
+    # M[line[0]] = Set(line[1:])
+    M[line[0]] = line[1:]
+    P.add(line[0])
+    L[line[0]] = 0.0
+
+# print M.__len__()
+# print P.__len__()
+# print L.__len__()
+
+
+# Updating L with outlinks count
+for inlinks in M.values():
+    for link in inlinks:
+        L[link] += 1.0
+
+# Updating S with sink nodes; i.e L[link] = 0
+for link, outlink_count in L.items():
+    if outlink_count == 0.0:
+        S.add(link)
+
+# total number of Pages
+N = float(len(P))
+
+# d is the PageRank damping/teleportation factor; use d = 0.85 as is typical
+d = 0.85
+
+for p in P:
+    PR[p] = 1.0 / N  # initial value
+
+count = 0
+
+while not(isConverged(count)):
+    # if j > 100:
+    #     break
+    sinkPR = 0.0
+    for p in S:
+        sinkPR += PR[p]
+
+    for p in P:
+        newPR[p] = (1.0 - d) / N
+        newPR[p] += (d * sinkPR) / N
+        for q in M[p]:
+            newPR[p] += (d * PR[q]) / L[q]
+
+    for p in P:
+        PR[p] = newPR[p]
+
+    count += 1
+
+sortedPR = sorted(PR.iteritems(), key=operator.itemgetter(1), reverse=True)
+
+i = 1
+
+# for link,score in sortedPR:
+#     if i>1000:
+#         break
+#     print str(i) + ". " + link + str(" : ") + str(score)
+#     i += 1
+
+for j in range(1000):
+    print sortedPR[j]
+
+f.close()
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # PagerankElastic
 Implementation of the Page Rank algorithm to compute Page Rank on various Topics.
+
+CS6200 Information Retrieval
+Homework4: Web graph computation
+
+Objective:
+    Compute link graph measures for each page crawled using the adjacency matrix. While you have to use the merged team
+    index, this assignment is individual (can compare with teammates the results)
+
+Page Rank - crawl
+    Compute the PageRank of every page in your crawl (merged team index). You can use any of the methods described in class: random walks (slow), transition matrix, algebraic solution etc. List the top 500 pages by the PageRank score. You can take a look at this PageRank pseudocode (for basic iteration method) to get an idea
+
+Page Rank - other graph
+    Get the graph linked by the in-links in file resources/wt2g_inlinks.txt.zip
+    Compute the PageRank of every page. List the top 500 pages by the PageRank score.
